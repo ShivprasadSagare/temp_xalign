74321bf05c2a486bbb36eee6f44f050fe36ea66f
ShivprasadSagare <shivprasad.sagare@gmail.com>
2022-07-22 14:35:04 +0530

    generate function removed from each validation step

--- role_specific_finetuner/model/model.py
+++ role_specific_finetuner/model/model.py
@@ -69,32 +69,33 @@ class FineTuner(pl.LightningModule):
         loss = self._step(batch)
         self.log("val_loss", loss, on_epoch=True)
 
-        input_text, pred_text, ref_text = self._generative_step(batch)
-        return {'val_loss': loss, 'input_text': input_text, 'pred_text': pred_text, 'ref_text': ref_text}
+        # input_text, pred_text, ref_text = self._generative_step(batch)
+        return {'val_loss': loss}#, 'input_text': input_text, 'pred_text': pred_text, 'ref_text': ref_text}
 
     def validation_epoch_end(self, outputs):
         # avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
         # self.log("epoch_val_loss", avg_loss)
 
-        input_text = []
-        pred_text = []
-        ref_text = []
-        for x in outputs:
-            input_text.extend(x['input_text'])
-            pred_text.extend(x['pred_text'])
-            ref_text.extend(x['ref_text'])
-        bleu = self.cal_bleu.corpus_score(pred_text, [ref_text])    
-        self.log("val_bleu", bleu.score)
+        # input_text = []
+        # pred_text = []
+        # ref_text = []
+        # for x in outputs:
+        #     input_text.extend(x['input_text'])
+        #     pred_text.extend(x['pred_text'])
+        #     ref_text.extend(x['ref_text'])
+        # bleu = self.cal_bleu.corpus_score(pred_text, [ref_text])    
+        # self.log("val_bleu", bleu.score)
 
-        random_indices = set([len(input_text)//i for i in range(2, 7)])
-        epoch_list = [self.trainer.current_epoch for i in range(len(random_indices))]
+        # random_indices = set([len(input_text)//i for i in range(2, 7)])
+        # epoch_list = [self.trainer.current_epoch for i in range(len(random_indices))]
 
-        input_text = [input_text[i] for i in random_indices]
-        pred_text = [pred_text[i] for i in random_indices]
-        ref_text = [ref_text[i] for i in random_indices]
+        # input_text = [input_text[i] for i in random_indices]
+        # pred_text = [pred_text[i] for i in random_indices]
+        # ref_text = [ref_text[i] for i in random_indices]
 
-        data = [i for i in zip(epoch_list, input_text, ref_text, pred_text)]
-        self.trainer.logger.log_text(key='validation_predictions', data=data, columns=['epoch', 'input_text', 'ref_text', 'pred_text'])
+        # data = [i for i in zip(epoch_list, input_text, ref_text, pred_text)]
+        # self.trainer.logger.log_text(key='validation_predictions', data=data, columns=['epoch', 'input_text', 'ref_text', 'pred_text'])
+        return 
 
     def test_step(self, batch, batch_idx):
         input_text, pred_text, ref_text = self._generative_step(batch)
