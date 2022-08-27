01500dbb84b1b43bc7e88281b6f3dc9555c70e43
ShivprasadSagare <shivprasad.sagare@gmail.com>
2022-08-27 15:23:12 +0530

    changes in hparams in run_finetune.sh

--- role_specific_finetuner/run_finetune.sh
+++ role_specific_finetuner/run_finetune.sh
@@ -9,25 +9,25 @@ checkpoint_path=None
 
 #For sanity checking whole pipeline with small data, pass argument 'yes'. For full run, pass 'no'
 python3 train.py \
---sanity_run yes \
---train_path 'data/xalign_HRT_only/train.csv' \
---val_path 'data/xalign_HRT_only/val.csv' \
---test_path 'data/xalign_HRT_only/test.csv' \
+--sanity_run no \
+--train_path 'data/role_ordered_xalign_HRT_only/train.csv' \
+--val_path 'data/role_ordered_xalign_HRT_only/val.csv' \
+--test_path 'data/role_ordered_xalign_HRT_only/test.csv' \
 --tokenizer_name_or_path 'google/mt5-small' \
 --max_source_length 384 \
 --max_target_length 128 \
---train_batch_size 4 \
---val_batch_size 4 \
---test_batch_size 4 \
+--train_batch_size 20 \
+--val_batch_size 20 \
+--test_batch_size 20 \
 --model_name_or_path 'google/mt5-small' \
 --learning_rate 3e-5 \
 --eval_beams 4 \
 --tgt_max_seq_len 128 \
 --checkpoint_path $checkpoint_path \
---gpus 2 \
---max_epochs 5 \
+--gpus 4 \
+--max_epochs 30 \
 --strategy 'ddp' \
 --log_dir 'experiments' \
 --project_name 'swft' \
---run_name 'xyz'
+--run_name 'finetuning_on-role_ordered_HRT_only_dataset'
 
