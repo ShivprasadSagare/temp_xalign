c89d2b9df925342c34f9ff659a3fcf57627a5f29
ShivprasadSagare <shivprasad.sagare@gmail.com>
2022-09-14 11:03:19 +0530

    changes for large model training

--- role_specific_finetuner/run_finetune.sh
+++ role_specific_finetuner/run_finetune.sh
@@ -1,11 +1,11 @@
 #Activate conda environment xalign_role.
 #Following line is added to resolve the error 'Your shell has not been properly configured to use 'conda activate'. 
 #Uncomment and Pass the appropriate anaconda/miniconda path in the below line, if you face the error.
-# source ~/miniconda3/etc/profile.d/conda.sh
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate xalign_role
 
 #If using checkpoint, pass appropriate arguments in code below, otherwise set checkpoint path to 'None' in below line
-checkpoint_path='pass ckpt path here'
+checkpoint_path=None
 
 #For sanity checking whole pipeline with small data, pass argument 'yes'. For full run, pass 'no'
 python3 train.py \
@@ -13,21 +13,21 @@ python3 train.py \
 --train_path 'data/xalign_unified_script/train.csv' \
 --val_path 'data/xalign_unified_script/val.csv' \
 --test_path 'data/xalign_unified_script/test.csv' \
---tokenizer_name_or_path 'google/mt5-small' \
+--tokenizer_name_or_path 'google/mt5-base' \
 --max_source_length 384 \
 --max_target_length 128 \
---train_batch_size 4 \
---val_batch_size 4 \
---test_batch_size 4 \
---model_name_or_path 'google/mt5-small' \
+--train_batch_size 20 \
+--val_batch_size 20 \
+--test_batch_size 20 \
+--model_name_or_path 'google/mt5-base' \
 --learning_rate 3e-5 \
 --eval_beams 4 \
 --tgt_max_seq_len 128 \
 --checkpoint_path $checkpoint_path \
---gpus 2 \
---max_epochs 5 \
+--gpus 4 \
+--max_epochs 30 \
 --strategy 'ddp' \
---log_dir '/scratch/experiments' \
+--log_dir 'experiments' \
 --project_name 'swft' \
---run_name 'multilingual-pretrained-finetuning'
+--run_name 'best_model_large_variant'
 
