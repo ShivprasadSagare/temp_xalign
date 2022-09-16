774c24fa317b344a5e0956fe420bb20111255f32
ShivprasadSagare <shivprasad.sagare@gmail.com>
2022-09-16 22:14:48 +0530

    update run_finetune.sh with new hparams

--- role_specific_finetuner/run_finetune.sh
+++ role_specific_finetuner/run_finetune.sh
@@ -6,26 +6,26 @@ checkpoint_path=None
 
 #For sanity checking whole pipeline with small data, pass argument 'yes'. For full run, pass 'no'
 python3 train.py \
---sanity_run yes \
+--sanity_run no \
 --train_path 'data/xalign_unified_script/train.csv' \
 --val_path 'data/xalign_unified_script/val.csv' \
 --test_path 'data/xalign_unified_script/test.csv' \
 --tokenizer_name_or_path 'google/mt5-small' \
 --max_source_length 384 \
 --max_target_length 128 \
---train_batch_size 4 \
---val_batch_size 4 \
---test_batch_size 4 \
+--train_batch_size 20 \
+--val_batch_size 20 \
+--test_batch_size 20 \
 --model_name_or_path 'google/mt5-small' \
 --learning_rate 3e-5 \
 --eval_beams 4 \
 --tgt_max_seq_len 128 \
 --checkpoint_path $checkpoint_path \
 --gpus 2 \
---max_epochs 3 \
+--max_epochs 30 \
 --strategy 'ddp' \
 --log_dir 'experiments' \
 --project_name 'swft' \
---run_name 'temp_data_scaling' \
+--run_name 'data_scaling' \
 --data_scaling_factor 0.001 
 
