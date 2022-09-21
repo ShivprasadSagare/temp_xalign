6ac3a9d04a646eb5158d521bf33b75c01460e84c
Shivprasad Sagare <61927602+ShivprasadSagare@users.noreply.github.com>
2022-09-21 13:44:22 +0530

    Update run_finetune.sh

--- role_specific_finetuner/run_finetune.sh
+++ role_specific_finetuner/run_finetune.sh
@@ -2,11 +2,11 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate xalign_role
 
 #If using checkpoint, pass appropriate arguments in code below, otherwise set checkpoint path to 'None' in below line
-checkpoint_path=None
+checkpoint_path=wandb
 
 #For sanity checking whole pipeline with small data, pass argument 'yes'. For full run, pass 'no'
 python3 train.py \
---sanity_run yes \
+--sanity_run no \
 --train_path 'data/role_ordered_xalign_HRT_only/train.csv' \
 --val_path 'data/role_ordered_xalign_HRT_only/val.csv' \
 --test_path 'data/role_ordered_xalign_HRT_only/test.csv' \
@@ -22,9 +22,9 @@ python3 train.py \
 --tgt_max_seq_len 128 \
 --checkpoint_path $checkpoint_path \
 --gpus 4 \
---max_epochs 30 \
+--max_epochs 20 \
 --strategy 'ddp' \
 --log_dir 'experiments' \
 --project_name 'swft' \
---run_name 'role-specific finetuning on role_ordered HRT only dataset'
+--run_name 'combined_model_extra_training_20_epochs_total_50'
 
